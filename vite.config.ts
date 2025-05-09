96f805b32e4f83ba0b6b1f142e241cdbc67ab461
Denis <97920897+Kyo473@users.noreply.github.com>
2025-05-10 01:48:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    allowedHosts: [".ngrok-free.app"],
-  },
   resolve: {
     alias: {
       "@ui": path.resolve(__dirname, "src/ui"),
